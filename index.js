cc3a4b20e28aa1d945f2bbd5f953836364274fc3
Parshv Limbad <103508696+ParshvLimbad@users.noreply.github.com>
2024-08-17 19:21:12 +0530

    test

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product.model");
 const productRoutes = require("./routes/product.route");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 //routes
 app.use("/api/products", productRoutes);
 
